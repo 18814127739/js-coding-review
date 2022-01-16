7a1b3d26158e8b7f3513aca0eda12df4f79c7203
tangjianming <tang.jianming@iwhalecloud.com>
2022-01-16 19:11:18 +0800

    update:完善深拷贝函数

--- basics/aop.js
+++ basics/aop.js
@@ -4,40 +4,40 @@
  */
 
 Function.prototype.before = function (callback) {
-  return (...ags) => { // 箭头函数，没有自己的this
-    const res = callback(...ags);
-    // this为原函数本身, 
-    this(...ags);
-    // 还可以可以把before函数得到的结果传给下一个执行环节
-    // this(res, ...ags);
-  }
+	return (...ags) => { // 箭头函数，没有自己的this
+		const res = callback(...ags);
+		// this为原函数本身,
+		this(...ags);
+		// 还可以可以把before函数得到的结果传给下一个执行环节
+		// this(res, ...ags);
+	}
 }
 
 Function.prototype.after = function (callbcak) {
-  return (...args) => {
-    this(...args);
-    callbcak(...args);
-  }
+	return (...args) => {
+		this(...args);
+		callbcak(...args);
+	}
 }
 
 function print(...args) {
-  console.log(...args);
+	console.log(...args);
 }
 
 // 添加前置函数
 const printBefore = print.before((a, b) => {
-  console.log(a + b);
-  return a + b;
+	console.log(a + b);
+	return a + b;
 });
 
 // 添加后置函数
 const printAfter = print.after((a, b) => {
-  console.log(a - b);
+	console.log(a - b);
 });
 
 // 添加前置和后置函数
 const printBeforeAfter = printBefore.after((a, b) => {
-  console.log(a - b);
+	console.log(a - b);
 })
 
 // test
--- basics/deepCopy.js
+++ basics/deepCopy.js
@@ -6,8 +6,16 @@ function deepCopy(obj) {
 	if (typeof obj !== 'object' || !obj) {
 		return obj;
 	}
+	if (['[object Date]', '[object RegExp]'].includes(Object.prototype.toString.call(obj))) {
+		// 正则和日期对象无法通过遍历复制
+		return obj;
+	}
 	let newObj = obj instanceof Array ? [] : {};
-	Object.keys(obj).forEach(k => {
+	// Object.keys(obj).forEach(k => {
+	// 	newObj[k] = typeof obj[k] === 'object' ? deepCopy(obj[k]) : obj[k];
+	// });
+	Object.getOwnPropertyNames(obj).forEach(k => {
+		// getOwnPropertyNames 可以获取对象的不可枚举属性，  keys不行
 		newObj[k] = typeof obj[k] === 'object' ? deepCopy(obj[k]) : obj[k];
 	});
 	return newObj;
@@ -24,9 +32,10 @@ const obj = {
 		console.log('copy');
 	},
 	d: null,
+	e: new RegExp("^abc"),
+	f: new Date(),
 }
 const newObj = deepCopy(obj);
 newObj.a.b = 2;
-console.log(newObj); // { a: { b: 2, c: [ 3, 4 ] }, b: undefined, c: [Function: c], d: null }
 console.log(obj); // { a: { b: 1, c: [ 3, 4 ] }, b: undefined, c: [Function: c], d: null }
-newObj.c(); // copy
+console.log(newObj); // { a: { b: 2, c: [ 3, 4 ] }, b: undefined, c: [Function: c], d: null }
